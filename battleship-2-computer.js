74eaa69fbb126fcb56289c02ce297cbb0dae65f7
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2023-10-30 12:02:01 +0100

    Update battleship-2-computer.js

--- battleship-2-computer.js
+++ battleship-2-computer.js
@@ -1,85 +1,106 @@
-// Komputer
-/*
-const computerRandShip: {
-    fullAreasBoard_AR: number[],
-    fillFullAreasBoard_AR_Func: Function,
-    compShips_AR: UserShipCor[],
-    onceShipArgs: (string | number | boolean)[],
-    createLimit: number,
-    createAvailableFields_Func: Function,
-    addCompShip_Func: Function,
-    direction_AR: string[],
-    shipLength_AR: number[]
-} = {
-    fullAreasBoard_AR: [],
+;
+var CompShipCor = /** @class */ (function () {
+    function CompShipCor(arg_1, arg_2, arg_3, arg_4, arg_5) {
+        this.number = arg_1;
+        this.length = arg_2;
+        this.direction = arg_3;
+        this.coordinates = arg_4;
+        this.hits = arg_5;
+        this.isSunken = false;
+    }
+    ;
+    return CompShipCor;
+}());
+;
+// Komputer - akcje:
+var computerRandShip = {
     compShips_AR: [],
+    fullAreasBoard_AR: [],
     onceShipArgs: [3, 'B'],
     createLimit: 0,
     direction_AR: ['B', 'R'],
     shipLength_AR: [2, 2, 2, 3, 3, 4, 5],
-    fillFullAreasBoard_AR_Func(): void {
-        for (let i: number = 0; i < 100; i++) {
+    fillFullAreasBoard_AR_Func: function () {
+        for (var i = 0; i < 100; i++) {
             this.fullAreasBoard_AR[i] = i;
-        };
+        }
+        ;
     },
-    addCompShip_Func(): void {
+    addCompShip_Func: function () {
         while (this.createLimit < this.shipLength_AR.length) {
             this.createLimit += 1;
             // Argumanty z buta wjeżdżające:
-            let number: number = this.createLimit;
-            let shipLength: number = this.shipLength_AR[this.createLimit - 1];
-            const shipDirection: string = this.direction_AR[Math.floor(Math.random() * 2)];
-            let availableFields: number[] = this.createAvailableFields_Func(shipLength, shipDirection);
+            var num = this.createLimit;
+            var lgt = this.shipLength_AR[this.createLimit - 1];
+            var dir = this.direction_AR[Math.floor(Math.random() * 2)];
+            var avlFld = this.createAvailableFields_Func(lgt, dir);
             //console.log(availableFields.length);
             // Częśc argumentów sepecjalnie do buildCheckera:
-            let firstCoor: number = Math.ceil(Math.random() * 99);
-            let fullIndexBoard = this.fullAreasBoard_AR;
-            const infoRecipient: HTMLElement = document.querySelector('div.none');
-            const isComp: boolean = true;
-            // Powrót: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-            let cor: number[] = shipColisions.checkShipColisions(firstCoor, shipLength, shipDirection, availableFields, fullIndexBoard, infoRecipient, isComp);
-            //console.log(cor);
-        };
+            var firstCoor = 0; // Random ustalane jest w builderCheckerze z powodu tego, iż współrzędne mogą się powtarzać i potrzeba ponownego wylosowania współrzędnej początkowej
+            var fullIdxBrd = this.fullAreasBoard_AR;
+            var infRcp = document.querySelector('div.none');
+            var isComp = true;
+            // Jeżeli wartością współrzędnych danego statku będzie "undefined" - powtórz losowanie:
+            var cor = shipColisions.checkShipColisions(firstCoor, lgt, dir, avlFld, fullIdxBrd, infRcp, isComp);
+            while (cor == undefined) {
+                cor = shipColisions.checkShipColisions(firstCoor, lgt, dir, avlFld, fullIdxBrd, infRcp, isComp);
+            }
+            ;
+            // Tworzenie tablicy trafień dla statku:
+            var hits = [];
+            for (var i = 0; i < cor.length; i++) {
+                hits[i] = false;
+            }
+            ;
+            var ship = new CompShipCor(num, lgt, dir, cor, hits);
+            this.compShips_AR.push(ship);
+        }
+        ;
+        console.log(this.compShips_AR);
     },
-    createAvailableFields_Func(shipLength, shipDirection) {
+    createAvailableFields_Func: function (shipLength, shipDirection) {
         // Dostępne pola:
-        let avlFldIdx: number = shipLength - 2;
-        let selectedTable_dir_B: number[] = dangerousFields_Obj.dir_B[avlFldIdx];
-        let selectedTable_dir_R: number[] = dangerousFields_Obj.dir_R[avlFldIdx];
-        let newArr: number[] = [];
+        var avlFldIdx = shipLength - 2;
+        var selectedTable_dir_B = dangerousFields_Obj.dir_B[avlFldIdx];
+        var selectedTable_dir_R = dangerousFields_Obj.dir_R[avlFldIdx];
+        var newArr = [];
         // Tworzenie nowej tablicy:
-        for (let i: number = 0; i < 100; i++) {
+        for (var i = 0; i < 100; i++) {
             newArr[i] = i;
-        };
+        }
+        ;
         // Czy wartość inputa "select" nie jest równa: "nie wybrano":
         // Kasowanie niedozwolonych indeksów w nowo-utworzonej tablicy, w zależności
         // od długości statku i jego położenia:
         if (shipDirection === 'B') {
-            for (let i: number = 0; i < 100; i++) {
-                for (let j: number = 0; j < selectedTable_dir_B.length; j++) {
+            for (var i = 0; i < 100; i++) {
+                for (var j = 0; j < selectedTable_dir_B.length; j++) {
                     if (newArr[i] == selectedTable_dir_B[j]) {
-                        let elLoc: number = newArr.indexOf(selectedTable_dir_B[j]);
+                        var elLoc = newArr.indexOf(selectedTable_dir_B[j]);
                         newArr.splice(elLoc, 1);
-                    } else {}
-                };
-            };
-        } else if (shipDirection === 'R') {
-            for (let i: number = 0; i < 100; i++) {
-                for (let j: number = 0; j < selectedTable_dir_R.length; j++) {
+                    }
+                    else { }
+                }
+                ;
+            }
+            ;
+        }
+        else if (shipDirection === 'R') {
+            for (var i = 0; i < 100; i++) {
+                for (var j = 0; j < selectedTable_dir_R.length; j++) {
                     if (newArr[i] == selectedTable_dir_R[j]) {
-                        let elLoc: number = newArr.indexOf(selectedTable_dir_R[j]);
+                        var elLoc = newArr.indexOf(selectedTable_dir_R[j]);
                         newArr.splice(elLoc, 1);
-                    } else {}
-                };
-            };
+                    }
+                    else { }
+                }
+                ;
+            }
+            ;
         }
         return newArr;
         //console.log(this.availableFields);    /*ARRAY_FIELDS CONSOLLOG*/
-//  },
-//};
-/*
+    },
+};
 computerRandShip.fillFullAreasBoard_AR_Func();
 computerRandShip.addCompShip_Func();
-
-
-*/ 
